12da413b9cdd1df1b7b3e335c74ab9e2a8372d9b
Joshua Montross <joshua.montross@gmail.com>
2013-01-13 14:58:02 -0800

    rails commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lessonoverflow::Application.config.secret_token = '0b2865a733665c3151277bf2545fc022f86f111c62186ac69e515acfc5b7b8ccab2cc9bba989fb94bfdc25144a3bd1c01e9ccac53e24f274857944396614424a'
