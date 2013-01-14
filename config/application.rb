a4618599fc5b92d0775a161defb82c904c6a40a5
Joshua Montross <joshua.montross@gmail.com>
2013-01-13 16:39:55 -0800

    ads everyting for the fonts

--- config/application.rb
+++ config/application.rb
@@ -9,6 +9,8 @@ if defined?(Bundler)
   # Bundler.require(:default, :assets, Rails.env)
 end
 
+
+
 module Lessonoverflow
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
@@ -58,5 +60,7 @@ module Lessonoverflow
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+    # ads assets like
+    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
